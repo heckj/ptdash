bdaddac8b0210b5c686e7ce544d2de12b5fcfcbb
Joe Heck <joseph.heck@emc.com>
2015-12-28 17:40:56 -0800

    adding code coverage

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ptdash
+
+[![Build Status](https://travis-ci.org/heckj/ptdash.svg?branch=master)](https://travis-ci.org/heckj/ptdash)
+[![Code Climate](https://codeclimate.com/github/heckj/ptdash/badges/gpa.svg)](https://codeclimate.com/github/heckj/ptdash)
+[![Issue Count](https://codeclimate.com/github/heckj/ptdash/badges/issue_count.svg)](https://codeclimate.com/github/heckj/ptdash)
+
+My stubbing out of a pivotal tracker dashboard using express
--- test/stub.js
+++ test/stub.js
@@ -1,10 +1,20 @@
 var chai = require('chai');
 var expect = chai.expect;
 chai.should();
+var request = require('supertest');
+var app = require('../app');
 
 describe("Stub", function() {
     it('should always pass', function() {
         "joe".should.be.a('string');
         expect(1).to.equal(1);
+    });
+});
+
+describe("ExpressApp", function() {
+    it('should respond', function(done) {
+        request(app)
+        .get('/')
+        .expect(200, done);
     });
 });
